bed10cf1b560330c9e53bc5c5090dfd65de6bcd7
raezlin <57278871+raezlin@users.noreply.github.com>
2020-08-14 21:36:42 -0400

    gentle update

--- mainScript.js
+++ mainScript.js
@@ -6,6 +6,12 @@ var currentWordList = [];
 var currentWordLength;
 var currentWord;
 var letterList = [];
+var wordString = '';
+
+
+var incorrectLetters = '';
+var correctLetters = '';
+var textArea = document.querySelector(".textAreaWrapper");
 
 userInput.addEventListener('input', updateValue);
 
@@ -18,14 +24,30 @@ $(document).ready(function () {
             words = data;
         }
     });
-    changeTextAreaWords(100);
+    initTextArea(100);
     currentWord = currentWordList[wordIndex];
     console.log('current word: ', currentWord);
     currentWordBreakDown(currentWord);
     console.log(letterList);
 });
 
+function initTextArea(length) {
+    let wordList = '';
 
+    for (let i = 0; i < length; i++) {
+        let randomWord = words[Math.floor(Math.random() * words.length)];
+        currentWordList.push(randomWord);
+        wordList += randomWord;
+        wordList += ' ';
+    }
+    wordString = wordList;
+    console.log(wordList);
+    textArea.appendChild(createChild('upcoming', '', wordString));
+}
+
+function updateResString(wordString) {
+    document.querySelector('.upcoming').innerHTML = wordString;
+}
 
 
 function currentWordBreakDown(word) {
@@ -40,48 +62,80 @@ function currentWordBreakDown(word) {
 }
 
 function updateValue(e) {
-
+    //this function is to record input letter, and call checkLetter
     var currentLetter = e.target.value;
-
-    // letterIndex = e.target.value.length - 1;
-
-    console.log(currentLetter);
     e.target.value = '';
     checkLetter(currentLetter);
     if (letterIndex < currentWordLength) {
         letterIndex += 1;
     }
     else if (letterIndex == currentWordLength) {
+        if (currentLetter == ' ') {
+            console.log('space');
+        }
         letterIndex = 0;
         wordIndex += 1;
         currentWord = currentWordList[wordIndex];
         currentWordLength = currentWord.length;
+        correctLetters += ' ';
+        textArea.insertBefore(createChild('correct', ' ', wordString), document.querySelector('.upcoming'));
         currentWordBreakDown(currentWordList[wordIndex]);
+
     }
-    console.log('letterIndex: ', letterIndex);
 
 }
 
 function checkLetter(letter) {
     if (letter == letterList[letterIndex]) {
-        console.log(`letter typed: ${letter}  expected letter: ${letterList[letterIndex]}`);
+        if (correctLetters.slice(-1) == " ") {
+            wordString = wordString.slice(2);
+        }
+        else {
+            wordString = wordString.slice(1);
+        }
+        correctLetters += letter;
+        updateResString(wordString);
+        textArea.insertBefore(createChild('correct', letter, wordString), document.querySelector('.upcoming'));
+        return true;
+
     }
+    else if (letter != letterList[letterIndex]) {
+        if (letterIndex < currentWordLength) {
+            incorrectLetters += letter;
+            wordString = wordString.slice(1);
+            textArea.insertBefore(createChild('incorrect', letter, wordString), document.querySelector('.upcoming'));
+            return false;
+        }
+        else {
+            if (letter != ' ') {
+                updateResString(wordString);
+                textArea.insertBefore(createChild('incorrect', letter, wordString), document.querySelector('.upcoming'));
+            }
+
+        }
+
+    }
+
 
 }
 
 
-function changeTextAreaWords(length) {
-    let wordList = '';
 
-    for (let i = 0; i < length; i++) {
-        let randomWord = words[Math.floor(Math.random() * words.length)];
-        currentWordList.push(randomWord);
-        wordList += randomWord;
-        wordList += ' ';
+
+function createChild(status, string, wordStringRes) {
+    let stringContent = document.createElement('letter');
+    stringContent.className = status;
+
+    if (status == 'correct') {
+        //if its the correct letter
+        stringContent.textContent = string;
     }
-    console.log(wordList);
-    $('.textArea').html(wordList);
+    else if (status == 'incorrect') {
+        stringContent.textContent = string;
+    }
+    else {
+        stringContent.textContent = wordStringRes;
+    }
+    return stringContent;
 }
-
-
 
